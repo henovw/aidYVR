a744fc4f79dda8c71633a3b6356a89784d78ab32
Henry van Weelderen <henryvanweelderen@gmail.com>
2025-11-18 14:09:09 -0800

    Add changes

--- src/components/MiniDesc.jsx
+++ src/components/MiniDesc.jsx
@@ -9,7 +9,7 @@ function MiniDesc(props) {
         <button className="exit" onClick={props.onClose}><img src="src/assets/xbutton.png"></img></button>
             <img className="minidesc-image" src={props.img}></img>
             </div>
-            <h1 style={{ display: "flex;" }}>{props.title}</h1>
+            <h1>{props.title}</h1>
             <br></br>
             <div class="categories">
                 
